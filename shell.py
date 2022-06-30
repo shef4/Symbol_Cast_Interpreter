c67e8fdf985112f9d72793c66b6ba2890e47c9c5
Shef <sefunmi88@gmail.com>
2022-06-30 09:49:51 -0400

    added Functions and loop structure

--- shell.py
+++ shell.py
@@ -5,3 +5,17 @@ while True:
 
     if error: print(error.as_string())
     else: print(result)
+    
+    # for loop
+    #VAR r = 1
+    #LOOP i = 1 TO 6 THEN VAR r =r*i
+    
+    
+    #while loop
+    #VAR i = 0 
+    #LOOP i < 10 THEN VAR i = i + 1
+    
+    #- FOR LOOP
+    ##LOOP i = 6 TO 1 THEN VAR i = i + 1
+    
+    #FUN add (a,b) -> a+b
